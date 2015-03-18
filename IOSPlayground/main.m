3332a856c113927b1f9bace2753b5730e13d45eb
zhgw01 <zhgw01@gmail.com>
2015-03-18 22:51:41 +0800

    Initial Commit

---
+++ IOSPlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOSPlayground
+//
+//  Created by Gongwei on 15/3/18.
+//  Copyright (c) 2015年 Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
